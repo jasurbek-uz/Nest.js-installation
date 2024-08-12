e496369f3e9a9518775727486a2bc13746851b71
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-08-12 12:59:28 +0900

    BBR: Setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// localhost:3000/detail
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+ public getHello(): string {
     return this.appService.getHello();
   }
 }
+
